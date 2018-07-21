bc704984ad99dc9e2f1095a76598ae7536c950fd
= <=>
2018-07-20 22:59:46 -0400

    Refactored code to get assets path

--- src/Helper.php
+++ src/Helper.php
@@ -6,15 +6,11 @@ class Helper
 {
     /**
      * Get the path to put the published assets.
-     * 
+     *
      * @return string
      */
     public static function getAssetsPath()
     {
-        if (is_null(config('coreui.assets_path'))) {
-            return public_path('admin/');
-        }
-
-        return config('coreui.assets_path');
+        return config('coreui.assets_path', public_path('admin/');
     }
 }
