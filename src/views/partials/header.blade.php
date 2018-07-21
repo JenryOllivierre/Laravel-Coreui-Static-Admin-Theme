af5baa2567a2051bb6b0671e34264882287e5bbd
= <=>
2018-07-21 00:40:49 -0400

    added the 'custom-styles' @section tag and modified the 'header-css' @section tag to be 'header-stylesheets'

--- src/views/partials/header.blade.php
+++ src/views/partials/header.blade.php
@@ -17,11 +17,16 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
     @show
 
-    <!-- Styles -->
-    @section('header-css')
+    <!-- Stylesheets -->
+    @section('header-stylesheets')
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('admin/coreui-static/css/style.min.css') }}" rel="stylesheet">
     @show
+
+    <!-- Custom Styles -->
+    <style type="text/css">
+        @yield('custom-styles')
+    </style>
 
     <!-- Header Vars -->
     <script type="text/javascript">@yield('header-vars')</script>
